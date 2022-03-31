772f213e9ff3ddbc9627d5f992843fa19af27f77
zsh <1659583514@qq.com>
2022-03-31 16:17:10 +0800

    git commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./main.css";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-import * as ElIcons from "@element-plus/icons";
-const web_server = createApp(App);
-Object.keys(ElIcons).forEach(function (key) {
-  web_server.component(ElIcons[key].name, ElIcons[key]);
+Vue.use(ElementUI);
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+  router,
+  store
 });
-web_server.config.devtools = true;
-web_server.use(store).use(router).use(ElementUI).mount("#app");
