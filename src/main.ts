0180e1a0e992ddf3b007b55be069e4fb7f230dc2
zsh <1659583514@qq.com>
2022-03-31 15:55:09 +0800

    git commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./main.css";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import * as ElIcons from "@element-plus/icons";
+const web_server = createApp(App);
+Object.keys(ElIcons).forEach(function (key) {
+  web_server.component(ElIcons[key].name, ElIcons[key]);
+});
+web_server.config.devtools = true;
+web_server.use(store).use(router).use(ElementPlus).mount("#app");
 
-createApp(App).use(store).use(router).mount("#app");
