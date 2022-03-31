5886d31721a801e0e1b38d1268f9ad1847c18e4d
zsh <1659583514@qq.com>
2022-03-31 18:16:48 +0800

    git commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/typescript/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
@@ -19,8 +18,8 @@ module.exports = {
       "error",
       {
         endOfLine: "auto",
-        trailingComma: "none"
-      }
-    ]
-  }
+        trailingComma: "none",
+      },
+    ],
+  },
 };
