f1a59c96945a09c99f4f95bc9abd9d17f2503745
zsh <1659583514@qq.com>
2022-05-17 15:46:03 +0800

    1文字禁止选中

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   padding-top: 80px;
+  -moz-user-select: none; //火狐
+  -webkit-user-select: none; //webkit浏览器
+  -ms-user-select: none; //IE10
+  -khtml-user-select: none; //早期浏览器
+  user-select: none;
 }
+
 * {
   list-style: none;
   font-family: "PHTB";
